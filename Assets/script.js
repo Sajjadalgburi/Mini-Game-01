572b1a658af51c1536e82284bc521764e44c6f66
Sajjadalgburi <dfgsajjad@gmail.com>
2023-11-26 21:03:11 -0700

    Added comments and fixed the if statment errors

--- Assets/script.js
+++ Assets/script.js
@@ -11,10 +11,12 @@ var playGame = function () {
   // Prompt the user to input their choice
   let userChoice = prompt("What would you like to choose? === R, P, or S === ");
 
+  // Check if the user cancels the prompt or does not provide any input
   if (!userChoice) {
     return;
   }
 
+  // Convert user choice to uppercase for case-insensitive comparison
   userChoice = userChoice.toUpperCase();
 
   // Generate a random index for the computer's choice using Math.random() and Math.floor()
@@ -23,23 +25,29 @@ var playGame = function () {
   // Retrieve the computer's choice based on the random index
   const computerChoice = options[computerChoiceIndex];
 
+  // Display the computer's choice to the user
   window.alert("The computer chose " + computerChoice + "!");
 
+  // Compare user's choice with the computer's choice to determine the winner
   if (userChoice === computerChoice) {
+    // It's a tie
     window.alert("It's a tie!");
     ties++; // Increment ties count
   } else if (
-    (userChoice === "R" && computerChoice === "P") ||
-    (userChoice === "P" && computerChoice === "S") ||
-    (userChoice === "S" && computerChoice === "R")
+    // User wins scenarios
+    (userChoice === "R" && computerChoice === "S") ||
+    (userChoice === "P" && computerChoice === "R") ||
+    (userChoice === "S" && computerChoice === "P")
   ) {
-    window.alert("Computer wins! You lose...");
-    losses++; // Increment losses countp
-  } else {
-    console.log("You win! Computer loses...");
+    window.alert("You win!");
     wins++; // Increment wins count
+  } else {
+    // Computer wins
+    window.alert("Computer wins!");
+    losses++; // Increment losses count
   }
 
+  // Display the current score
   window.alert("Wins: " + wins + " Losses: " + losses + " Ties: " + ties);
 };
 
